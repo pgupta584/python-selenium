09d8e25f3c7dbb9a7d38178c4c3a6768ece71f62
pankaj.gup <pankajgup@zeta.tech>
2023-09-07 22:19:01 +0530

    fixture at class level

--- sample_pytest_test/test_fixture_8.py
+++ sample_pytest_test/test_fixture_8.py
@@ -3,7 +3,7 @@ import pytest
 
 # Apply only once at class level based on requirements
 
-@pytest.mark.usefixtures("login_to_gmail")
+@pytest.mark.usefixtures("login_to_gmail_class")
 class TestFixture1:
     @pytest.mark.send_email1
     def test_send_email(self):
