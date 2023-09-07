85dcbc819d7409b85ca18e74a06bc50b3b164a8f
pankaj.gup <pankajgup@zeta.tech>
2023-09-07 22:00:49 +0530

    Conftest-fixture

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
-# import pytest
+import pytest
+
 
 # --> fixture must be inside conftest file only --> name should be same
 
@@ -7,16 +8,17 @@
 # Let's see one example
 
 # If we want this test should run only once before class
-# @pytest.fixture(scope="class")
-# def login_to_gmail():
-#     print("Login to Gmail Success")
-#
-# # Use of yield --> like after method/class
-# @pytest.fixture(scope="class")
-# def login_to_gmail():
-#     print("Login to Gmail Success")
-#     yield
-#     print("Logout is Success")
+@pytest.fixture(scope="class")
+def login_to_gmail_class():
+    print("Login to Gmail Success")
+
+
+# Use of yield --> like after method/class
+@pytest.fixture(scope="function")
+def login_to_gmail_method():
+    print("Login to Gmail Success")
+    yield
+    print("Logout is Success")
 
 # If we want this test should run after every method
 # @pytest.fixture(scope="function")
@@ -24,3 +26,6 @@
 #     print("Login to Gmail Success")
 
 # Now we need to pass this fixture to function, will create one more test file to understand
+
+# scope --> function, Means before every method it will run like before method in testNg
+# scope --> class, Means before every class it will run like before class in testNg
--- sample_pytest_test/test_fixture_7.py
+++ sample_pytest_test/test_fixture_7.py
@@ -6,11 +6,11 @@ import pytest
 # Let's see one example
 class TestFixture:  # Test Name should start/End with Test to follow best practice
     @pytest.mark.send_email
-    def test_send_email(self, login_to_gmail):
+    def test_send_email(self, login_to_gmail_method):
         assert True
         print("Email sent is Success")
 
     @pytest.mark.send_email
-    def test_send_email_with_attachments(self, login_to_gmail):
+    def test_send_email_with_attachments(self, login_to_gmail_method):
         assert True
         print("Email sent with attachments is Success")
